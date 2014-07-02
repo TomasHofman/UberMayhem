703ec6162a6fb28bca50eeda19ae938007744c2b
Tomas Hofman <tomas.hofman.cz@gmail.com>
2014-07-02 19:49:08 +0200

    Integrated with LWJGL

--- app/src/main/scala/mayhem/Application.scala
+++ app/src/main/scala/mayhem/Application.scala
@@ -1,5 +1,24 @@
+import org.lwjgl.LWJGLException
+import org.lwjgl.opengl.{DisplayMode, Display}
+
 object Application {
   def main(args: Array[String]) {
     println("Uber-Mayhem Welcomes You")
+
+    try {
+      Display.setDisplayMode(new DisplayMode(600, 600))
+      Display.create()
+    } catch {
+      case e: LWJGLException =>
+      {
+        e.printStackTrace()
+        sys.exit(0)
+      }
+    }
+
+    while (!Display.isCloseRequested) {
+      Display.update()
+    }
+    Display.destroy()
   }
 }
